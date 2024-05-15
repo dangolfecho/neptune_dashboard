78b49f8ba0e5754beb5922f21183d1a330acfb7a
dangolfecho <55153800+dangolfecho@users.noreply.github.com>
2024-05-15 16:07:26 +0530

    Wrote gitignore and initial pie chart

--- neptune/dashboard/views.py
+++ neptune/dashboard/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import plotly.express as px 
+import numpy as np
+import pandas as pd
 
 # Create your views here.
+
+def hello(request):
+    df = pd.read_excel("D:\\Neptune\\Sales- Dashboard.xlsx")
+    region_labels = ['Central', 'East', 'South', 'West']
+    values = []
+    for i in region_labels:
+        values.append(df.loc[df['Region'] == i, 'Sales'].sum())
+    fig = pgo.pie(df['Region'], values=values, names=region_labels)
+    div = fig.to_html(full_html=False)
+    context = {'pie': div}
+    return render(request, 'home.html', context);
