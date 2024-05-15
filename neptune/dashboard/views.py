7c6a00926c53356d829f5df32e9f6e999f31bbb7
dangolfecho <55153800+dangolfecho@users.noreply.github.com>
2024-05-16 00:51:41 +0530

    Added styling to the dashboard

--- neptune/dashboard/views.py
+++ neptune/dashboard/views.py
@@ -1,17 +1,42 @@
 from django.shortcuts import render, redirect
-import plotly.express as px 
+import plotly.graph_objects as go
+import plotly.express as px
 import numpy as np
 import pandas as pd
 
 # Create your views here.
 
+night_colors = ['rgb(56, 75, 126)', 'rgb(18, 36, 37)', 'rgb(34, 53, 101)',
+                'rgb(36, 55, 57)', 'rgb(6, 4, 4)']
+
 def hello(request):
     df = pd.read_excel("D:\\Neptune\\Sales- Dashboard.xlsx")
-    region_labels = ['Central', 'East', 'South', 'West']
-    values = []
-    for i in region_labels:
-        values.append(df.loc[df['Region'] == i, 'Sales'].sum())
-    fig = pgo.pie(df['Region'], values=values, names=region_labels)
+    regions = ['Central', 'East', 'South', 'West']
+    sales_by_region = []
+    for i in regions:
+        sales_by_region.append(df.loc[df['Region'] == i, 'Sales'].sum())
+    fig = go.Figure(data=[go.Pie(labels=regions, values=sales_by_region, marker_colors=night_colors)])
+
+    fig.update_layout(
+        paper_bgcolor="LightSteelBlue",
+        #paper_bgcolor="#0e0f2e",
+        plot_bgcolor="#0e0f2e",
+        margin=dict(l=20, r=20, t=20, b=20),
+        width=500,
+        height=500
+    )
+    '''
+    fig.add_trace(
+        go.Pie(df['Region'], values=sales_by_region, names=regions,
+            color_discrete_sequence=px.colors.sequential.ice,
+            template="plotly_dark"
+        )
+    )
+    fig = px.pie(df['Region'], values=sales_by_region, names=regions,
+                color_discrete_sequence=px.colors.sequential.ice,
+                template="plotly_dark")
+    fig2 = 
+    '''
     div = fig.to_html(full_html=False)
     context = {'pie': div}
-    return render(request, 'home.html', context);
+    return render(request, 'home.html', context)
