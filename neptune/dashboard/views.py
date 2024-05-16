d03172c544148372e6b176d6305c41bd268e423d
dangolfecho <55153800+dangolfecho@users.noreply.github.com>
2024-05-16 19:13:58 +0530

    Modified styling and added funnel

--- neptune/dashboard/views.py
+++ neptune/dashboard/views.py
@@ -9,6 +9,9 @@ import pandas as pd
 night_colors = ['rgb(56, 75, 126)', 'rgb(18, 36, 37)', 'rgb(34, 53, 101)',
                 'rgb(36, 55, 57)', 'rgb(6, 4, 4)']
 
+master_width = 1000
+master_height = 500
+
 def hello(request):
     df = pd.read_excel("D:\\Neptune\\Sales- Dashboard.xlsx")
     regions = ['Central', 'East', 'South', 'West']
@@ -22,8 +25,8 @@ def hello(request):
         #paper_bgcolor="#0e0f2e",
         plot_bgcolor="#0e0f2e",
         margin=dict(l=20, r=20, t=20, b=20),
-        width=500,
-        height=500
+        width=master_width,
+        height=master_height
     )
 
     fig1 = go.Figure()
@@ -42,27 +45,29 @@ def hello(request):
         go.Scatter(
             x = months,
             y = sales_by_month,
-            fill = 'tozeroy'
+            fill = 'tozeroy',
+            visible=True
         )
     )
     fig1.add_trace(
         go.Scatter(
             x = quarters,
             y = sales_by_quarter,
-            fill = 'tozeroy'
+            fill = 'tozeroy',
+            visible=False
         )
     )
 
     buttons1 = [
         dict(
-            label='View by Quarter',
-            method='update',
-            args=[{'visible': [False, True]}, {'title': 'Quarterly Sales'}]
-        ),
-        dict(
             label='View by Month',
             method='update',
             args=[{'visible': [True, False]}, {'title': 'Monthly Sales'}]
+        ),
+        dict(
+            label='View by Quarter',
+            method='update',
+            args=[{'visible': [False, True]}, {'title': 'Quarterly Sales'}]
         )
     ]
 
@@ -71,8 +76,8 @@ def hello(request):
         #paper_bgcolor="#0e0f2e",
         plot_bgcolor="#0e0f2e",
         margin=dict(l=20, r=20, t=20, b=20),
-        width=500,
-        height=500,
+        width=master_width,
+        height=master_height,
         updatemenus=[
             dict(
                 type='buttons',
@@ -87,23 +92,32 @@ def hello(request):
         ]
     )
 
-
     sales_by_region_qty = []
     for i in regions:
         sales_by_region_qty.append(df.loc[df['Region'] == i, 'Quantity ordered new'].sum())
+    temp = [[sales_by_region[i], regions[i]] for i in range(4)]
+    temp = sorted(temp)
+    sorted_sales_by_region = [temp[i][0] for i in range(4)]
+    sorted_region_1 = [temp[i][1] for i in range(4)]
+    temp = [[sales_by_region_qty[i], regions[i]] for i in range(4)]
+    temp = sorted(temp)
+    sorted_sales_by_region_qty = [temp[i][0] for i in range(4)]
+    sorted_region_2 = [temp[i][1] for i in range(4)]
     fig2 = go.Figure()
     fig2.add_trace(
         go.Bar(
-            x=sales_by_region,
-            y=regions,
-            orientation='h'
+            x=sorted_sales_by_region,
+            y=sorted_region_1,
+            orientation='h',
+            visible=True
         )
     )
     fig2.add_trace(
         go.Bar(
-            x=sales_by_region_qty,
-            y=regions,
-            orientation='h'
+            x=sorted_sales_by_region_qty,
+            y=sorted_region_2,
+            orientation='h',
+            visible=False
         )
     )
 
@@ -111,12 +125,12 @@ def hello(request):
         dict(
             label='View by Revenue',
             method='update',
-            args=[{'visible': [False, True]}, {'title': 'Revenue'}]
+            args=[{'visible': [True, False]}, {'title': 'Revenue'}]
         ),
         dict(
             label='View by Quantity',
             method='update',
-            args=[{'visible': [True, False]}, {'title': 'Quantity'}]
+            args=[{'visible': [False, True]}, {'title': 'Quantity'}]
         )
     ]
 
@@ -125,8 +139,8 @@ def hello(request):
         #paper_bgcolor="#0e0f2e",
         plot_bgcolor="#0e0f2e",
         margin=dict(l=20, r=20, t=20, b=20),
-        width=500,
-        height=500,
+        width=master_width,
+        height=master_height,
         updatemenus=[
             dict(
                 type='buttons',
@@ -138,12 +152,35 @@ def hello(request):
                 y=1.15,
                 yanchor='top'
             )
-        ],
-        xaxis={'categoryorder': 'total descending'}
+        ]
     )
+
+    # Funnel
+    
+    df1 = pd.read_excel("D:\\Neptune\\Sales- Dashboard.xlsx", sheet_name="Returns")
+    total_sales_count = df.shape[0]
+    total_return_count = df1.shape[0]
+    stages = ["Sale Initiated", "Sale Completed without a return"]
+    fig3 = go.Figure()
+    fig3.add_trace(go.Funnel(
+        x = [total_sales_count, (total_sales_count-total_return_count)],
+        y = stages
+    ))
+
+    fig3.update_layout(
+        paper_bgcolor="LightSteelBlue",
+        #paper_bgcolor="#0e0f2e",
+        plot_bgcolor="#0e0f2e",
+        margin=dict(l=20, r=20, t=20, b=20),
+        width=master_width,
+        height=master_height
+    )
+
 
     div = fig.to_html(full_html=False)
     div1 = fig1.to_html(full_html=False)
     div2 = fig2.to_html(full_html=False)
-    context = {'pie': div, 'trend': div1, 'bar': div2}
+    div3 = fig3.to_html(full_html=False)
+
+    context = {'pie': div, 'trend': div1, 'bar': div2, 'funnel': div3}
     return render(request, 'home.html', context)
